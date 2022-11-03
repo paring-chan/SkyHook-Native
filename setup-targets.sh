538fffdf185c8ba70091368ddd14c92359c6401f
Pariring <paring@pikokr.dev>
2022-11-03 23:51:14 +0900

    chore: add darwin aarch64 target

--- setup-targets.sh
+++ setup-targets.sh
@@ -9,5 +9,8 @@ rustup target add x86_64-pc-windows-gnu
 rustup target add i686-unknown-linux-gnu
 rustup target add i686-pc-windows-gnu
 
+# ARM
+rustup target add aarch64-apple-darwin
+
 # sudo pacman -S mingw-w64-binutils mingw-w64-gcc mingw-w64-headers \
 #                 clang gcc zlib libmpc mpfr gmp
