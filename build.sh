d49ea3d104a0bb04e82001aca9c98f2fcf70657d
Pariring <paring@pikokr.dev>
2022-11-03 19:40:16 +0900

    chore: remove i686 linux target

--- build.sh
+++ build.sh
@@ -12,11 +12,9 @@ cargo build --release --target x86_64-pc-windows-gnu
 
 
 # i686
-cargo build --release --target i686-unknown-linux-gnu
 cargo build --release --target i686-pc-windows-gnu
 
 cp target/x86_64-unknown-linux-gnu/release/libskyhook.so out/x86_64
 cp target/x86_64-pc-windows-gnu/release/skyhook.dll out/x86_64
 
-cp target/i686-unknown-linux-gnu/release/libskyhook.so out/i686
 cp target/i686-pc-windows-gnu/release/skyhook.dll out/i686
