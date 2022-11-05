329887be5857c1d7c886628c649bd4bdb99d1a12
Pariring <paring@pikokr.dev>
2022-11-05 13:42:52 +0900

    fix: remove cross compiling

--- build.sh
+++ build.sh
@@ -12,13 +12,8 @@ mkdir -p out/i686
 
 if [[ $platform == 'Linux' ]]; then
   cargo build --release --target x86_64-unknown-linux-gnu
-  cargo build --release --target x86_64-pc-windows-gnu
-  cargo build --release --target i686-pc-windows-gnu
 
   cp target/x86_64-unknown-linux-gnu/release/libskyhook.so out/x86_64
-  cp target/x86_64-pc-windows-gnu/release/skyhook.dll out/x86_64
-
-  cp target/i686-pc-windows-gnu/release/skyhook.dll out/i686
 elif [[ $platform == 'Darwin' ]]; then
   cargo build --release --target x86_64-apple-darwin
   cargo build --release --target aarch64-apple-darwin
